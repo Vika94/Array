b648d7bd8e15780d892c7b94893ac6612d530a93
Vika94 <vikuscha1994@mail.ru>
2021-06-30 20:29:10 +0300

    Initial commit

---
+++ src/MultiArray.java
@@ -0,0 +1,61 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class MultiArray {
+    public static void main(String[] args) {
+        /*System.out.print("Введите размер массива: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Random random = new Random();
+        int mas[][] = new int[n][n];
+        int sum = 0;
+        for (int[] numb : mas) {
+            for (int i = 0; i < numb.length; i++) {
+                numb[i] = random.nextInt(50);
+            }
+        }
+        for (int[] numb : mas) {
+            for (int i: numb) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < mas.length; i++) {
+            if (mas[i][i]%2==0){
+               sum+=mas[i][i];
+            }
+        }
+        System.out.println(sum);
+
+         */
+       /* System.out.print("Введите размер массива: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Random random = new Random();
+        int[][] mas = new int[n][n];
+        int sum = 0;
+        for (int i = 0; i < mas.length; i++) {
+            for (int j = 0; j < mas.length; j++) {
+                mas[i][j] = random.nextInt(50);
+            }
+        }
+        for (int[] numb : mas) {
+            for (int i : numb) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < mas.length; i++) {
+            if (mas[i][i] % 2 == 0) {
+                sum += mas[i][i];
+            }
+        }
+
+        */
+    }
+}
+
+
+
+
+
